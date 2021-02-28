a6f7e27ddb1a7b1d5a7df4d1a0b6abbbb0ed6338
kishorshrestha21 <iamkishorshrestha@gmail.com>
2021-02-28 16:45:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-templatedrivenform
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-templatedrivenform)
---
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: "app-template-driven-form",
+  templateUrl: "./template-driven-form.component.html",
+  styleUrls: ["./template-driven-form.component.css"]
+})
+export class TemplateDrivenFormComponent implements OnInit {
+  constructor() {}
+
+@ViewChild('myForm') myForm:NgForm;
+  ngOnInit() {}
+
+  onSubmit(form:NgForm) {
+    console.log(this.myForm);
+  }
+}

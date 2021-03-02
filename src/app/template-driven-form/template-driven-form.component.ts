68c86f0403b8726bad9c62b07e83d2f95c86b467
kishorshrestha21 <iamkishorshrestha@gmail.com>
2021-03-02 14:03:25 -0500

    3 files modified

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -9,17 +9,47 @@ import { NgForm } from "@angular/forms";
 export class TemplateDrivenFormComponent implements OnInit {
   constructor() {}
 
-@ViewChild('myForm') myForm:NgForm;
+  @ViewChild("myForm") myForm: NgForm;
 
-defualtStylist='Tom';
-// username='';
-  ngOnInit() {
+  defualtStylist = "Tom";
+  username = "";
+  defaultGender = "Male";
 
-  }
 
-  onSubmit(form:NgForm) {
+
+  ngOnInit() {}
+// for radio
+  genders = [{ id: "1", value: "Male" }, { id: "2", value: "Female" }];
+
+submited:boolean=false;
+formData={
+  username:'',
+  email:'',
+  course:'',
+  gender:''
+}
+  onSubmit(form: NgForm) {
     console.log(this.myForm);
-
+    this.submited=true;
+    this.formData.username = this.myForm.value.username;
+    this.formData.email = this.myForm.value.email;
+    this.formData.course = this.myForm.value.course;
+    this.formData.gender = this.myForm.value.gender;
   }
 
+  onSetValue(){
+    this.myForm.setValue({
+      username:'kishor',
+      email:'kishor@gmail.com',
+      stylist:'Ram',
+      gender:'Male'
+
+    })
+  }
+
+  onpatchValue() {
+    this.myForm.form.patchValue({
+      username: "kishor shrestha"
+    });
+  }
 }
